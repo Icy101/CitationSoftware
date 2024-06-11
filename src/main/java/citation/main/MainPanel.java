c0abc3347c173bc8bdee7f7fd80f4062cfc43d29
Icy101 <super.mohanverma@gmail.com>
2024-06-11 13:49:52 -0600

    changed commits

--- src/main/java/citation/main/MainPanel.java
+++ src/main/java/citation/main/MainPanel.java
@@ -17,15 +17,17 @@ public class MainPanel extends JFrame {
     }
 
     private void components(){
+       // settings for the main window
     setTitle("Citation Software");
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setSize(500,450);
+    setSize(600,700);
     setLocationRelativeTo(null);
     setContentPane(new cite());
 
     }
 
     public static void main(String[] args){
+        // bringing fonts and colors from FlatLaf
         FlatRobotoFont.install();
         FlatLaf.registerCustomDefaultsSource("citation.themes");
         UIManager.put("defaultFont", new Font(FlatRobotoFont.FAMILY,Font.PLAIN,13));
